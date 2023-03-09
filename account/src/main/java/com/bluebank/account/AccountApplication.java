30afb1d126b64a53be1b64f90496617cff8feb7b
Salma Yassin <syassin@uplandsoftware.com>
2023-03-09 03:00:39 +0200

    Adding dummy data

--- account/src/main/java/com/bluebank/account/AccountApplication.java
+++ account/src/main/java/com/bluebank/account/AccountApplication.java
@@ -1,11 +1,31 @@
 package com.bluebank.account;
 
+import com.bluebank.account.model.Account;
+import com.bluebank.account.model.AccountType;
+import com.bluebank.account.service.AccountService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class AccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountApplication.class,args);
+    }
+
+    @Bean
+    CommandLineRunner run(AccountService accountService) {
+        return args -> {
+            Account account = new Account(null, AccountType.SAVINGS, LocalDateTime.now(),new BigDecimal(200), 1L);
+            Account account1 = new Account(null, AccountType.CHECKING, LocalDateTime.now(),new BigDecimal(8000), 1L);
+            Account account2 = new Account(null, AccountType.SAVINGS, LocalDateTime.now(),new BigDecimal(0), 2L);
+            accountService.create(account);
+            accountService.create(account1);
+            accountService.create(account2);
+        };
     }
 }
--- customer/src/main/java/com/bluebank/customer/CustomerApplication.java
+++ customer/src/main/java/com/bluebank/customer/CustomerApplication.java
@@ -1,11 +1,29 @@
 package com.bluebank.customer;
 
+import com.bluebank.customer.model.Customer;
+import com.bluebank.customer.service.CustomerService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(CustomerService customerService) {
+        return args -> {
+            Customer customer = new Customer(1L, "Salma","Yassin","syassin@gmail.com");
+            Customer customer1 = new Customer(2L, "Mohamed","Wael","mwael@gmail.com");
+            Customer customer2 = new Customer(3L, "Sohila","Abdou","sabdou@gmail.com");
+           customerService.registerCustomer(customer);
+           customerService.registerCustomer(customer1);
+           customerService.registerCustomer(customer2);
+        };
     }
 }
